e2a2a610032df53767667a4395435fc89192d3ac
Ajay Kumar Saraswat <ajaykumarsaraswat9045@gmail.com>
2025-01-23 16:12:59 +0530

    creating user_models and routes

---
+++ model/user.js
@@ -0,0 +1,63 @@
+const mongoose = require("mongoose");
+// const { createHmac ,randomBytes} = require('node:crypto');
+// const { createTokenforUser } = require("../service/authentication");
+
+const UserSchema = new mongoose.Schema({
+    fullName : {
+        type : String,
+        required : true,
+    },
+    email : {
+        type : String,
+        required : true,
+        unique : true,
+    },
+
+    salt : {
+        type : String,
+       
+    },
+    password : {
+        type : String,
+        required : true,
+
+    },
+    role : {
+        type : String,
+        enum : ["USER","ADMIN"],
+        default : "USER",
+    }
+
+},{timestamps : true})
+
+// UserSchema.pre('save', function(next) {
+//     const user = this;
+//     if(!user.isModified("password")) return ;
+//     const salt = randomBytes(16).toString();
+//     const hashedPassword = createHmac('sha256', salt)
+//                .update(user.password)
+//                .digest('hex');
+//                this.salt = salt;
+//                this.password = hashedPassword;
+
+
+//     next();
+//   });
+
+//   UserSchema.static('matchPasswordandGenerateToken',async function(email,password){
+//     const user = await this.findOne({email}); 
+//     if(!user) throw new Error('User dont found')
+//     const salt =  user.salt;
+//     const hashedPassword = user.password;
+//     const userProvidedHash = createHmac('sha256', salt)
+//     .update(password)
+//     .digest('hex');
+//    if(hashedPassword !==userProvidedHash) throw new Error('User dont found');
+//     const token = createTokenforUser(user);
+   
+//     return token;
+//  });
+
+const User = mongoose.model("user",UserSchema);
+
+module.exports = User;
